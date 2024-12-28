b33ba7d4a935ba49bbc1f93f2b8586f1b90e12f2
Rijoan <Rijoan@RIJOANFARDOUS>
2024-12-28 20:11:46 +0600

    Add project files.

---
+++ OrderMS.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderMS.Domain.Enums
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Completed,
+        Cancelled,
+    }
+}
